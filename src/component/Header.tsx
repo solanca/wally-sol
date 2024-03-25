4d05e96717624847cae2906f455ad42371f7ca52
sparqxi <davidkim052894@gmail.com>
2024-03-25 02:20:02 -0700

    header position

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -7,14 +7,14 @@ const Header = (_props: Props) => {
   return (
     <AppBar position='absolute' sx={{backgroundColor:"transparent",top:20,px:{md:30,sm:5},boxShadow:'none' }}>
         <Toolbar>
+           <img src='assets/logo.png' width={"20%"}/>
+           <Box flexGrow={1}/>
            <Button>
             <img src='assets/svg/x.svg'/>
            </Button>
            <Button sx={{ml:4}}>
             <img src='assets/svg/te.svg'/>
            </Button>
-           <Box flexGrow={1}/>
-           <img src='assets/logo.png' width={"20%"}/>
         </Toolbar>
     </AppBar>
   )
