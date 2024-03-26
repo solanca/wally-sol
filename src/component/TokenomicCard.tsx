e6328b0eaab54cdda81695d7872198ea340b393b
sparqxi <davidkim052894@gmail.com>
2024-03-26 04:04:23 -0700

    wallynomics

---
+++ src/component/TokenomicCard.tsx
@@ -0,0 +1,46 @@
+import { Box, Card, CardContent, CardMedia, Typography, styled } from '@mui/material'
+import {motion} from 'framer-motion'
+
+type Props = {
+  id:number,
+  text:string,
+  x:number
+}
+
+const Root = styled(Card)(({theme}) => ({
+  border:'2px solid black',
+  borderRadius:20,
+  padding:10,
+  margin:theme.breakpoints.down("sm") ? 20:0
+
+}))
+
+const TokenomicCard = ({id,text,x}: Props) => {
+  return (
+    <motion.div  
+    animate={{
+      y: [-10 + x, 0, -10 +x],
+    }}
+    transition={{
+      duration: 2,
+      ease: "easeInOut",
+      times: [0, 0.5, 1],
+      repeat: Infinity,
+    }}>
+
+    <Root>
+      <CardMedia image={`assets/illustrations/${id}.png`} component="img"
+        height="360" />
+      <CardContent>
+        <Typography variant='h4' textAlign={"center"}>{text}</Typography>
+        
+      </CardContent>
+{/* 
+      <img src="assets/illustrations/1.png"/> */}
+      {/* </CardContent> */}
+    </Root>
+    </motion.div>
+  )
+}
+
+export default TokenomicCard
--- src/component/Tokenomics.tsx
+++ src/component/Tokenomics.tsx
@@ -1,32 +1,62 @@
+import { Box, Grid, Typography } from "@mui/material";
 import Divider from "./Divider";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
+import TokenomicCard from "./TokenomicCard";
 type Props = {};
 
 const Tokenomics = (_props: Props) => {
   return (
     <div className="tokenomics" id="wallynomics">
-      <div className="tokenomics__container container">
-      <motion.div
-              animate={{
-                y: [-30, 0, -30],
-              }}
-              transition={{
-                duration: 2,
-                ease: "easeInOut",
-                times: [0, 0.5, 1],
-                repeat: Infinity,
-              }}
-            >
-              {/* <img src="assets/website assets/3.png" alt="" /> */}
-        <img
-          className="tokenomics__boat"
-          src="/assets/img/tokenomics/1.png"
-          // width={"30%"}
-          alt=""
-        />
-            </motion.div>
+       <Grid container justifyContent={"center"} mt={2}>
+
+          <Typography color={"azure"} variant="h2" textAlign={"center"}>Wallynomics</Typography>
+      </Grid>
+      {/* <Grid container alignItems={"center"}>
+        <Grid item md={6} sm={12} xs={12}>
+
+      <img src="assets/img/hero/1.png" width={"80%"}/>
+        </Grid>
+        <Grid item md={6} sm={12} xs={12}> */} 
+          {/* <Grid container> */}
+
+          {/* <Box sx={{position:'relative'}}>
+
+          <Typography variant="h2" textAlign={"center"}>Total Supply:   1T</Typography>
+          <Typography variant="h2" textAlign={"center"} mt={2}>LP:    Burned</Typography>
+          <Typography variant="h2" textAlign={"center"} mt={2}>Taxes:    0</Typography>
+          <Typography variant="h2" textAlign={"center"} mt={2}>Mint:    Revoked</Typography>
+         
+
+          <img src="assets/solos/2.png" width={"30%"} style={{position:'absolute',bottom:-80,right:10,transform:'scaleX(-1)'}}/>
       
-        <div className="tokenomics__col">
+          </Box> */}
+          {/* </Grid> */}
+        {/* </Grid>
+      </Grid> */}
+      {/* <img src="assets/banner/banner1.png" /> */}
+      {/* <div className="tokenomics__container container"> */}
+        
+        {/* <motion.div
+          animate={{
+            y: [-30, 0, -30],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.5, 1],
+            repeat: Infinity,
+          }}
+        > */}
+          {/* <img src="assets/website assets/3.png" alt="" /> */}
+          {/* <img
+            className="tokenomics__boat"
+            src="/assets/img/tokenomics/1.png"
+            // width={"30%"}
+            alt=""
+          />
+        </motion.div> */}
+
+        {/* <div className="tokenomics__col">
           <span className="tokenomics__badge">Total supply: 1 T</span>
           <h2 className="tokenomics__title">Wallynomics</h2>
           <div className="tokenomics__items">
@@ -58,8 +88,18 @@ const Tokenomics = (_props: Props) => {
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
+<Grid container mb={4} spacing={4} mt={4}>
+  <Grid item md={3} sm={6}  xs={12}> <TokenomicCard id={1} text="Total Supply: 1T" x={20}/></Grid>
+  <Grid item md={3} sm={6}   xs={12}> <TokenomicCard id={2} text="LP: Burned" x={0}/></Grid>
+  <Grid item md={3} sm={6}   xs={12}> <TokenomicCard id={3}  text="Mint: Revoked" x={20}/></Grid>
+  <Grid item md={3} sm={6}   xs={12}> <TokenomicCard id={4} text="Taxes: 0" x={0}/></Grid>
+  {/* <Grid item md={4}></Grid>
+  <Grid item md={4}></Grid>
+  <Grid item md={4}></Grid> */}
+</Grid>
+     
       <div className="marque tokenomics__marque tokenomics__marque_bottom">
         <Divider />
       </div>
