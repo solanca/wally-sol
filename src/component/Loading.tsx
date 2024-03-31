d88265003de499d2c64e0b27c628fe34b6d6116d
sparqxi <davidkim052894@gmail.com>
2024-03-31 09:20:46 -0700

    loading bar updated

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,71 +1,92 @@
-import { Grid, LinearProgress, linearProgressClasses, styled } from "@mui/material";
+import {
+  Box,
+  Grid,
+  LinearProgress,
+  Typography,
+  linearProgressClasses,
+  styled,
+} from "@mui/material";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 type Props = {
-    loading:boolean
+  loading: boolean;
 };
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
-    borderRadius: 5,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 5,
-      backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-    },
-  }));
-const Loading = ({loading}: Props) => {
-    const [progress, setProgress] = useState(0);
+const BorderLinearProgress = styled(LinearProgress)(() => ({
+  height: 36,
+  borderRadius: 24,
+  border: "3px solid black",
+  [`&.${linearProgressClasses.root}`]: {
+    backgroundColor: "transparent !important",
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 24,
+    borderRight: "3px solid black",
+
+    backgroundColor: "#5BE1E3",
+  },
+}));
+
+const ProgressLabel = styled(Typography)(() => ({
+  position: "absolute",
+  left: "52%",
+  top: "50%",
+  transform: "translate(-50%, -50%)",
+  fontWeight: "bold",
+  color: "#1F559E",
+}));
+
+const Loading = ({ loading }: Props) => {
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
+        if (oldProgress >= 100) {
+          clearInterval(timer);
+          return 100; 
         }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
+        return Math.round(Math.min(oldProgress + 6.67, 100));
       });
-    }, 200);
-
+    }, 150);
+  
     return () => {
       clearInterval(timer);
     };
   }, []);
+  
   return (
-    // // <Root>
-    //   {/* <Box position={"relative"}> */}
-    //   {/* <div> */}
-      <Grid container justifyContent={"center"} flexDirection={"column"} alignItems={"center"} sx={{height:"100vh",display:loading?'flex':'none'}}>
+    <Grid
+      container
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      sx={{ height: "100vh", display: loading ? "flex" : "none" }}
+    >
+      <motion.img
+        animate={{ rotate: [10, 0, 10] }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          times: [0, 0.5, 1],
+          repeat: Infinity,
+        }}
+        src="assets/website assets/1.png"
+        style={{ height: 400, width: 350 }}
+      />
 
-          {/* <div> */}
+      <Box position={"relative"}>
+        <BorderLinearProgress
+          sx={{ width: 300 }}
+          color="secondary"
+          variant="determinate"
+          value={progress}
+        />
+        <ProgressLabel color="#FFAF59" variant="subtitle1">
+          {progress}%
+        </ProgressLabel>
+      </Box>
+    </Grid>
     
-    <motion.img  animate={{ rotate: [10, 0, 10] }}
-              transition={{
-                duration: 1,
-                ease: "easeInOut",
-                times: [0, 0.5, 1],
-                repeat: Infinity,
-              }} src="assets/website assets/1.png" style={{height:400,width:300}}/>
-          {/* </div> */}
-            {/* <motion.img
-              src="assets/website assets/1.png"
-              animate={{ rotate: [20, 0, 10] }}
-              transition={{
-                duration: 2,
-                ease: "easeInOut",
-                times: [0, 0.6, 1],
-                repeat: Infinity,
-              }}
-              initial={{ scale: 0.4 }}
-            ></motion.img> */}
-          {/* </div> */}
-          <BorderLinearProgress sx={{width:300}} color='secondary' variant="determinate" value={progress} />
-      </Grid>
-    //   {/* </Box> */}
-
-    // // </Root>
   );
 };
 
